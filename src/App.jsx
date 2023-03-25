a48da36237475276342cf3e0268cd6d109793a28
Shweta Kanekar <kanekarshweta10@gmail.com>
2023-03-25 20:29:08 +0530

    Created basic editor and previewer.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import './App.css';
 
 function App() {
-  return <div>Markdown Previewer</div>;
+  return (
+    <div id="container">
+      <h1>Markdown Previewer</h1>
+      <div id="main-section">
+        <div id="editor-section">
+          <h2>Editor</h2>
+          <form>
+            <textarea name="editor" id="editor"></textarea>
+          </form>
+        </div>
+        <div id="preview-section">
+          <h2>Preview</h2>
+          <div id="preview"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
